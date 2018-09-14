cac2d70e196390f1cc2798596b666606addb7755
Xavier Duncan <xduncan@pm.me>
2018-09-14 15:39:01 -0400

    add Board component... app works!

---
+++ main.js
@@ -0,0 +1,8 @@
+// smart components
+class Board extends React.Component {
+    render() {
+        return <h1>App Works!</h1>
+    };
+};
+
+ReactDOM.render(<Board/>, document.querySelector('.app'));
